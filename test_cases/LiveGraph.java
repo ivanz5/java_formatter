74bd9e9d786b3dbf73e47d984911a2c711806c4b
ivanzhur <vzhur98@gmail.com>
2018-12-17 00:15:48 +0200

    Add test cases

---
+++ test_cases/LiveGraph.java
@@ -0,0 +1,47 @@
+package com.ivanzhur.shakeunlock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LiveGraph extends Graph {
+    Graph mainGraph;
+
+    static final int NEED_MORE_POINTS = -1;
+    static final int GRAPHS_NOT_EQUAL = 0;
+    static final int GRAPHS_EQUAL = 1;
+    static final int IN_PROGRESS = 2;
+
+    public LiveGraph(Graph graphToCompareWith){
+        super();
+        mainGraph = graphToCompareWith;
+    }
+
+    public int addPoint(GraphPoint point){
+        // Add new point
+        points.add(point);
+        numPoints++;
+        // Return -1 if not enough points to calculate peak
+        if (numPoints < 3) return NEED_MORE_POINTS; // Possible to return IN_PROGRESS
+
+        // If previous point is a peak, add it's index to 'peaks'
+        if (GraphPoint.isPeak(points.get(numPoints-3).value, points.get(numPoints-2).value, points.get(numPoints-1).value)){
+            peaks.add(numPoints-2);
+            numPeaks++;
+        }
+
+        // Graph can be equal to 'main' if it has not less peaks than 'main'
+        // Use 'compareGraphs' to compare them, if equal return success
+        double peaksDiffRatio = (double)(numPeaks-mainGraph.numPeaks)/mainGraph.numPeaks;
+        if (peaksDiffRatio >= 0 && compareGraphs(this, mainGraph))
+            return GRAPHS_EQUAL;
+
+        // If not equal and too much peaks return fail
+        if (peaksDiffRatio >= MAX_NUM_PEAKS_DIFF_RATIO)
+            return GRAPHS_NOT_EQUAL;
+
+        // Too early to compare OR
+        // Not equal at this moment but peaks number is acceptable
+        // so can be equal next time
+        return IN_PROGRESS;
+    }
+}
---
+++ test_cases/output.java
@@ -0,0 +1,131 @@
+package pack;
+
+import java.something;
+
+public class Foo extends Bar implements A, B, C {
+    public int[] X = new int[]{1, 3, 5, 7, 9, 11};
+
+    public static final String STR="HELLO";
+
+    synchronized {
+        int sync = 7;
+    }
+
+    public static void huh() {
+        int kk = 7;
+    }
+
+    class CLO {
+    }
+
+
+    public void foo(boolean a, int x, int y, int z)throws Ex1, Ex2, Ex3 {
+        if (2 < 3) { //KEK
+            return;
+        }
+
+        // Line
+        // Another line
+
+
+
+        if (1 == 1) { // KEEEEEEK
+            int k = - 6; // INT
+            int lol = k;
+            bool b = ! true;
+
+
+            int a = 6;
+            System.out.println(a);
+        }
+        foo();
+
+        if (0 > - 1)break; // LOL
+
+        if (1 == 1 || 2 == 2) {
+        } else if (5 > 0) {
+            String s="ggg";
+        } else {
+            int a;
+        }
+        int z = 9;
+
+        if (7 == 7) {
+            break;
+        }
+
+        /*do {
+            x+ +;
+        }  */while (false);
+
+        do x++;
+        while (true);
+
+        print(this :: onclick);
+
+        switch(a) {
+            case 0:
+                a++;
+                break;
+
+            case 1:
+                a += 100;
+            case 8:
+                a--;
+                break;
+
+            case 2:
+                a += 2;
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
+
+        while (a > b) {
+            a++;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + 10);
+        }
+
+        for (String s:list) {
+            print(s);
+        }
+
+        while (x < 10000);
+
+        while (x < 50000)x++;
+
+        for (int i = 0; i < 5; i++)System.out.println(i);
+
+
+        try {
+            int a = 8;
+            if (a == 8) {
+                a /= 0;
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+        }
+
+
+        try (Scanner scanner = new Scanner(new File("test.txt"))){
+            while (scanner.hasNext()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
+        }
+    }
+
+    int a = 8;
+
+    // This is barr
+    public void barr() {
+        return;
+    }
+}
